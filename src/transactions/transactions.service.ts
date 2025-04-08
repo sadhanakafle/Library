af4b9b2b0e161df7001df8562bb6328bb2aedcbe
unknown <sadhanakafle42@gmail.com>
2025-04-08 13:42:20 +0545

    transaction api completed

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -5,20 +5,12 @@ import {
 } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, ReservationType } from '@prisma/client';
 
 @Injectable()
 export class TransactionsService {
   constructor(private readonly prisma: PrismaClient) {}
   async create(createTransactionDto: CreateTransactionDto) {
-    let member = await this.prisma.user.findUnique({
-      where: {
-        id: createTransactionDto.member_id,
-      },
-    });
-    if (!member) {
-      throw new NotFoundException('member not found');
-    }
     let book = await this.prisma.book.findUnique({
       where: {
         id: createTransactionDto.book_id,
@@ -29,74 +21,60 @@ export class TransactionsService {
     }
 
     if((book.quantity as number)<=0){
-
+      throw new BadRequestException('book is not available');
     }
-    const BorrowedBookscount = await this.prisma.transaction.count({
+
+    let member = await this.prisma.user.findUnique({
       where: {
-        user_id: createTransactionDto.user_id,
-        type: 'BORROW',
+        id: createTransactionDto.member_id,
       },
     });
-    return await this.prisma.$transaction(async (prisma) => {//tcl
-      if (createTransactionDto.type === 'BORROW') {
-        if (BorrowedBookscount >= 5) {
-          throw new BadRequestException('User has reached the borrowing limit');
-        }
-
-        if (book.quantity <= 0) {
-          throw new BadRequestException('Book is out of stock.');
-        }
-
-        await prisma.book.update({
-          where: { id: createTransactionDto.book_id },
-          data: { quantity: book.quantity - 1 },
-        });
-      }
-      if (createTransactionDto.type === 'RETURN') {
-        const BORROW = await this.prisma.transaction.findFirst({
-          where: {
-            user_id: createTransactionDto.user_id,
-            type: 'BORROW',
-            book_id: createTransactionDto.book_id,
-          },
-        });
-
-        await prisma.book.update({
-          where: { id: createTransactionDto.book_id },
-          data: { quantity: book.quantity + 1 },
-        });
-      }
-
-      return await prisma.transaction.create({
-        data: createTransactionDto,
+    if (!member) {
+      throw new NotFoundException('member not found');
+    }
+    const existingtransaction = await this.prisma.transaction.findFirst({
+      where:{
+        book_id:createTransactionDto.book_id,
+        member_id:createTransactionDto.member_id,
+        type: ReservationType.BORROW,
+      },
+    });
+    if(existingtransaction){
+      throw new BadRequestException("book is already borrrowed by the memeber")
+    }
+    return this.prisma.$transaction(async (prisma) => {//tcl
+      const transaction = await prisma.transaction.create({
+        data:createTransactionDto,
       });
+      await prisma.book.update({
+        where: { id: createTransactionDto.book_id },
+        data: { quantity: {
+          ...(createTransactionDto.type ===  ReservationType.BORROW?{decrement:1}:{increment:1})
+        } },
+      });
+       return transaction;
     });
   }
+        
   findAll() {
     return this.prisma.transaction.findMany();
   }
   async findOne(id: number) {
-    const data = await this.prisma.transaction.findUnique({
+    const transaction = await this.prisma.transaction.findUnique({
       where: { id },
     });
-    if (!data) {
+    if (!transaction) {
       throw new NotFoundException('transaction not found');
     }
-    return data;
+    return transaction;
   }
   async update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    let transaction = await this.prisma.transaction.findUnique({
-      where: { id },
+    await this.findOne(id);
+    return this.prisma.transaction.update({
+      where:{id},
+      data:updateTransactionDto,
     });
-
-    if (!transaction) {
-      throw new NotFoundException('Transaction not found');
     }
-    return await this.prisma.transaction.update({
-      where: { id },
-      data: updateTransactionDto,
-    });
-  }
   async remove(id: number) {
     await this.findOne(id);
     return this.prisma.transaction.delete({
